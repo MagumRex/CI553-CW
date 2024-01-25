f89d53182922c223451d5a202031ac8069971685
capit <capit@Eclipse>
2024-01-25 11:54:31 +0000

    Added test stubs to the new/modified classes

---
+++ clients/cashier/BetterCashierModelTest.java
@@ -0,0 +1,19 @@
+package clients.cashier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BetterCashierModelTest {
+
+	@Test
+	void testBetterCashierModel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMakeBasket() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ clients/cashier/CashierClientTest.java
@@ -0,0 +1,14 @@
+package clients.cashier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CashierClientTest {
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
